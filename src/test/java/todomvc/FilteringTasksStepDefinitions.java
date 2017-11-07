3160d7ea4cd7c19e4fdd01114525995c1f4f1553
John Ferguson Smart <john.smart@wakaleo.com>
2017-11-07 11:03:52 +0000

    Added pending steps for the filtering tasks feature

---
+++ src/test/java/todomvc/FilteringTasksStepDefinitions.java
@@ -0,0 +1,37 @@
+package todomvc;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import java.util.List;
+
+public class FilteringTasksStepDefinitions {
+
+
+    @Given("^that Jane has a todo list containing Buy some milk, Walk the dog$")
+    public void that_Jane_has_a_todo_list_containing_Buy_some_milk_Walk_the_dog() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^she has completed the task called 'Walk the dog'$")
+    public void she_has_completed_the_task_called_Walk_the_dog() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^she filters her list to show only Active tasks$")
+    public void she_filters_her_list_to_show_only_Active_tasks() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^she filters her list to show only Completed tasks$")
+    public void she_filters_her_list_to_show_only_Completed_tasks() throws Exception {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+}
