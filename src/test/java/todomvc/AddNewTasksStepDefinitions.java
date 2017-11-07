5d8aa2c82f32db269863d95ac470d50afbb12a3d
John Ferguson Smart <john.smart@wakaleo.com>
2017-11-07 08:59:58 +0000

    Preparation for lesson 10

--- src/test/java/todomvc/AddNewTasksStepDefinitions.java
+++ src/test/java/todomvc/AddNewTasksStepDefinitions.java
@@ -1,5 +1,6 @@
 package todomvc;
 
+import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -15,29 +16,31 @@ public class AddNewTasksStepDefinitions {
 
     @Given("^that (?:.*) has an empty todo list$")
     public void that_James_has_an_empty_todo_list() throws Exception {
-        todoUser.startsWithAnEmptyTodoList();
+        // TODO
+        throw new PendingException();
     }
 
     @Given("^that (?:.*) has a list containing (.*)$")
     public void has_a_list_containing(List<String> tasks) throws Exception {
-        todoUser.startsWithAnEmptyTodoList();
-        tasks.forEach(
-                task -> todoUser.adds_a_task(task)
-        );
+        // TODO
+        throw new PendingException();
     }
 
     @When("^s?he adds '(.*)' to (?:his|her) list$")
     public void he_adds_to_his_list(String taskName) throws Exception {
-        todoUser.adds_a_task(taskName);
+        // TODO
+        throw new PendingException();
     }
 
     @Then("^'(.*)' should be recorded in (?:his|her) list$")
     public void should_be_recorded_in_his_list(String taskName) throws Exception {
-        todoUser.should_see_task(taskName);
+        // TODO
+        throw new PendingException();
     }
 
     @Then("^(?:his|her) todo list should contain (.*)$")
     public void list_should_contain(List<String> tasks) throws Exception {
-        todoUser.should_see_tasks(tasks);
+        // TODO
+        throw new PendingException();
     }
 }
